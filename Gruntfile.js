711ac85dd4dc43d4c122b2946b387eb481766b89
drom <alex.drom@gmail.com>
2014-05-18 12:33:19 -0700

    grunt with nw builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,41 +4,40 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js', 'src/*.js']
+      all: ['Gruntfile.js']
     },
-    concat: {
-      dist: {
-        src: ['src/JsonML.js', 'src/WaveDrom.js', 'src/Save.js'],
-        dest: 'build/build.js',
-      },
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'build/build.js',
-        dest: 'build/WaveDrom.js'
+    copy: {
+      editor: {
+        files: [
+          {expand: true, src: [
+            'WaveDrom.js',
+            'editor.html', 'tutorial.html',
+            'images/ic_*.png', 'images/favicon.ico', 'logo.png',
+            'skins/*', 'css/*', 'scripts/*'
+          ], dest: 'WaveDromEditor_build/', filter: 'isFile'},
+          {expand: true, flatten: true, src:['WaveDromEditor/package.json'], dest: 'WaveDromEditor_build/', filter: 'isFile'},
+        ]
       }
     },
     nodewebkit: {
       options: {
         build_dir: './nw_builds',
+        keep_nw: true,    
         mac: true,
         win: true,
         linux32: true,
         linux64: true
       },
-      src: ['./WaveDromEditor/*']
+      src: ['./WaveDromEditor_build/*']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+//  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-node-webkit-builder');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'nodewebkit']);
+  grunt.registerTask('default', ['jshint', 'copy', 'nodewebkit']);
 };
