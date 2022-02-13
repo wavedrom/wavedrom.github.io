0fa178ca8ce6dc5a0bf0a9b60dd827b6e97d7f64
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2022-02-13 17:34:53 -0500

    v2.9.1

--- build-wde.sh
+++ build-wde.sh
@@ -26,4 +26,4 @@ cp scripts/* src/scripts
 
 curl https://raw.githubusercontent.com/drom/simple-nwjs-app/master/build.sh -o build.sh
 
-bash build.sh --name wavedrom-editor --version v2.7.0
+bash build.sh --name wavedrom-editor --version v2.9.1
